fd4ae5c2fcc9456f0e2521d6d913d8417c892039
eduardoajs2020 <eduardoajs2020@gmail.com>
2020-12-29 23:22:59 -0300

    angular alterators1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,41 @@
+import { CourseListComponent } from './courses/course-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
+import { Error404Component } from './core/component/error-404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: '**', component: Error404Component
+      },
+    ]),
+    CourseModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,37 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
-  selector: 'app-course-list',
-  templateUrl: './course-list.component.html'
+    templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
-  courses: Course[] = [];
-  ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular: Forms',
-        imageUrl: '',
-        price: 99.99,
-        code: 'XPS-8796',
-        duration: 120,
-        rating: 4.5,
-        releaseDate: 'December, 2, 2019'
-      },
-      {
-        id: 2,
-        name: 'Angular: HTTP',
-        imageUrl: '',
-        price: 45.99,
-        code: 'LKL-1094',
-        duration: 80,
-        rating: 4,
-        releaseDate: 'December, 2, 2019'
 
-      }
-    ];
+    filteredCourses: Course[] = [];
 
-  }
+    _courses: Course[] = [];
+
+    _filterBy: string;
+
+    constructor(private courseService: CourseService) { }
+
+    ngOnInit(): void {
+        this.retrieveAll();
+    }
+
+    retrieveAll(): void {
+        this.courseService.retrieveAll().subscribe({
+            next: courses => {
+                this._courses = courses;
+                this.filteredCourses = this._courses;
+            },
+            error: err => console.log('Error', err)
+        })
+    }
+
+    deleteById(courseId: number): void {
+        this.courseService.deleteById(courseId).subscribe({
+            next: () => {
+                console.log('Deleted with success');
+                this.retrieveAll();
+            },
+            error: err => console.log('Error', err)
+        })
+    }
+
+    set filter(value: string) {
+        this._filterBy = value;
+
+
+        // tslint:disable-next-line: max-line-length
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+    }
+
+    get filter() {
+        return this._filterBy;
+    }
 
 }
